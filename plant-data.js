8d396003b4efeab5746667e9655dce830acbdf5e
Abriel Cleaver <abrielcleaver@gmail.com>
2021-11-02 11:27:11 -0700

    add some plant-data

---
+++ plant-data.js
@@ -0,0 +1,36 @@
+
+export default [
+    {
+        'id': 1,
+        'common-name': 'Swiss Cheese plant',
+        'genus': 'Monstera  Deliciosa',
+        'care-level': '3',
+        'family': 'Aracea',
+        'plant-type': 'Tropical, climbing evergreen',
+        'mature-size': '3 ft. tall',
+        'sun-exposure': 'Bright indirect sunlight, partial shade',
+        'soil-type': 'Peat based potting soil, well drained',
+        'soil-pH': 'acid or neutral',
+        'bloom-time': 'Mid-summer',
+        'native-area': 'Central America',
+        'propagation': 'Yes',
+        'toxicity': 'Toxic to cats and dogs ',
+    },
+    {
+        'id': 2,
+        'common-name': 'Baby Rubber Plant/Radiator Plant',
+        'genus': 'Peperomia',
+        'care-level': '2',
+        'family': 'Piperacea',
+        'plant-type': 'Tropical Perennial',
+        'mature-size': '6-12 in. tall',
+        'sun-exposure': 'Full or partial sunlight',
+        'soil-type': 'Moist, well drained soil',
+        'soil-pH': 'Neutral to acidic',
+        'bloom-time': 'Summer',
+        'native-area': 'Central America, South America, & Caribbean',
+        'propagation': 'Yes',
+        'toxicity': 'Non-toxic. Pet-friendly.',
+    }
+    
+];
