9bb7045df2354401c1fda84511146088f0aa0489
Abriel Cleaver <abrielcleaver@gmail.com>
2021-11-05 00:07:16 -0700

    started potential foundation of sorting functioon

---
+++ sort-plants.js
@@ -0,0 +1,23 @@
+//  import plants from './plant-data.js'
+// import findById ?
+
+// create array to be sorted
+// const plantData = ['easy', 'easier', 'easiest'];
+
+// sort by value -- pass through array to get value (care-level) used for sorting into the temporary array
+// const mapped = plantData.map((id, plants) => {
+    // return { plants, value: care-level(id) }
+// })
+
+// sort temporary array
+// mapped.sort((a,b) => {
+    // move through the temporary array to achieve the correct order of care-level
+    // if a is less than b by the ordering criteria -> < sorts a before b?
+//    if (a.value > b.value) {
+//    return 1;
+// }
+//  if (a.value < b.value) {
+//      return -1;
+// }
+// return 0;
+// });
